11377e9365116ce921b437b69a665decc7d22830
Muthu Nambi <muhtu142001@gmail.com>
2025-09-17 22:08:06 +0530

    Update: new version of React app

--- src/Firebase.jsx
+++ src/Firebase.jsx
@@ -1,16 +1,15 @@
-// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDdJF7w4p1kUvArCpBhVMsTv4UImky7qsY",
-  authDomain: "shalom-builders-40aa2.firebaseapp.com",
-  projectId: "shalom-builders-40aa2",
-  storageBucket: "shalom-builders-40aa2.appspot.com", // <-- fixed
-  messagingSenderId: "663535551381",
-  appId: "1:663535551381:web:c9a6a0ed51f5e94ca4ec9c",
-  measurementId: "G-CXZ83BBCWL"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
